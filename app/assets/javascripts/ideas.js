d46f5c4ee59911de69814296c07357c475663729
Jill Donohue <jillmd501@gmail.com>
2016-02-02 11:47:17 -0700

    Start CRUD functionality * Create functionality

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -45,6 +45,32 @@ function fetchIdeas() {
   })
 }
 
+function createIdea() {
+  $("#create-idea").on("click", function() {
+    var ideaParams = {
+      idea: {
+        title: $("#idea-title").val()
+        body: $("#idea-body").val()
+      }
+    }
+
+    $("#idea-title").val()
+    $("#idea-body").val()
+
+    $.ajax({
+      type:    "POST",
+      url:     "/api/v1/ideas.json",
+      data:    ideaParams,
+      success: function(newIdea) {
+        renderIdea(newIdea)
+      },
+      error: function(xhr) {
+        console.log(xhr.responseText)
+      }
+    })
+  })
+};
+
 var thumbsUp = {
   genius: "genius",
   plausible: "genius",
